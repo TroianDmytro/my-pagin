80baa15ecd0670aebb194c691fd18d687d832e08
TroianDmytro <146668649+TroianDmytro@users.noreply.github.com>
2024-07-08 11:15:40 +0300

    #Test
    creat component Test

---
+++ src/components/TestGit/TestGit.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TestGitWrapper } from './TestGit.styled';
+
+const TestGit = () => (
+ <TestGitWrapper>
+    TestGit Component
+ </TestGitWrapper>
+);
+
+TestGit.propTypes = {};
+
+TestGit.defaultProps = {};
+
+export default TestGit;
